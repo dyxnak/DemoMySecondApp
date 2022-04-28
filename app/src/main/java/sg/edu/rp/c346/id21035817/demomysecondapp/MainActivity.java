48d6e244991a416d453669e342a1d9ab54dd68b7
21035817 <21035817@myrp.edu.sg>
2022-04-28 20:30:47 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21035817/demomysecondapp/MainActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id21035817.demomysecondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
